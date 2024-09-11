054022c15b269d5f714bac4a610d9f67c22d4664
Artyom-Gusarov <135129391+Artyom-Gusarov@users.noreply.github.com>
2024-09-11 17:51:55 +0300

    add member types definition, implement iterator
    type of iterator: RandomAccessIterator

--- chunk_vector.hpp
+++ chunk_vector.hpp
@@ -10,11 +10,125 @@ template <
     typename Alloc = std::allocator<T>>
 class chunk_vector {
 private:
+    template <bool is_const>
+    class chunk_iterator
+        : public std::iterator<std::random_access_iterator_tag, T> {
+    private:
+        using Owner = chunk_vector<T, chunk_size, Alloc>;
+        using Value = std::conditional_t<is_const, const T, T>;
+        Owner *m_chunk_vector_ptr;
+        std::size_t m_index;
+
+    public:
+        chunk_iterator(Owner ptr = nullptr, std::size_t index = 0)
+            : m_chunk_vector_ptr(ptr), m_index(index) {
+        }
+
+        bool operator==(const chunk_iterator &other) const noexcept {
+            return m_chunk_vector_ptr == other.m_chunk_vector_ptr &&
+                   m_index == other.m_index;
+        }
+
+        bool operator!=(const chunk_iterator &other) const noexcept {
+            return m_chunk_vector_ptr != other.m_chunk_vector_ptr ||
+                   m_index != other.m_index;
+        }
+
+        Value &operator*() const noexcept {
+            return m_chunk_vector_ptr->operator[](m_index);
+        }
+
+        Value *operator->() const noexcept {
+            return get_ptr_by_index(m_index);
+        }
+
+        chunk_iterator &operator++() noexcept {
+            ++m_index;
+            return *this;
+        }
+
+        chunk_iterator operator++(int) noexcept {
+            chunk_iterator tmp = *this;
+            ++m_index;
+            return tmp;
+        }
+
+        chunk_iterator &operator--() noexcept {
+            --m_index;
+            return *this;
+        }
+
+        chunk_iterator operator--(int) noexcept {
+            chunk_iterator tmp = *this;
+            --m_index;
+            return tmp;
+        }
+
+        chunk_iterator &operator+=(std::ptrdiff_t n) noexcept {
+            m_index += n;
+            return *this;
+        }
+
+        chunk_iterator &operator-=(std::ptrdiff_t n) noexcept {
+            m_index -= n;
+            return *this;
+        }
+
+        chunk_iterator operator+(std::ptrdiff_t n) const noexcept {
+            return chunk_iterator(m_chunk_vector_ptr, m_index + n);
+        }
+
+        chunk_iterator operator-(std::ptrdiff_t n) const noexcept {
+            return chunk_iterator(m_chunk_vector_ptr, m_index - n);
+        }
+
+        std::ptrdiff_t operator-(const chunk_iterator &other) const noexcept {
+            return m_index - other.m_index;
+        }
+
+        Value &operator[](std::ptrdiff_t n) const noexcept {
+            return m_chunk_vector_ptr->operator[](m_index + n);
+        }
+
+        bool operator<(const chunk_iterator &other) const noexcept {
+            return m_index < other.m_index;
+        }
+
+        bool operator>(const chunk_iterator &other) const noexcept {
+            return m_index > other.m_index;
+        }
+
+        bool operator<=(const chunk_iterator &other) const noexcept {
+            return m_index <= other.m_index;
+        }
+
+        bool operator>=(const chunk_iterator &other) const noexcept {
+            return m_index >= other.m_index;
+        }
+    };
+
     std::size_t v_size;
     std::size_t v_capacity;
     std::vector<T *> v_chunks;
 
+    T *get_ptr_by_index(std::size_t index) {
+        return v_chunks[index / chunk_size] + index % chunk_size;
+    }
+
 public:
+    using value_type = T;
+    using allocator_type = Alloc;
+    using size_type = std::size_t;
+    using difference_type = std::ptrdiff_t;
+    using reference = value_type &;
+    using const_reference = const value_type &;
+    using pointer = typename std::allocator_traits<Alloc>::pointer;
+    using const_pointer = typename std::allocator_traits<Alloc>::const_pointer;
+    using iterator = chunk_iterator<false>;
+    using const_iterator = chunk_iterator<true>;
+    using reverse_iterator = std::reverse_iterator<iterator>;
+    using const_reverse_iterator = std::reverse_iterator<const_iterator>;
+
     chunk_vector() : v_size(0), v_capacity(0) {
     }
 
